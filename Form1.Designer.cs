ece22daf87559ac6534de41199662d979207a98e
Николай Тюфекчиев <a.lrminatora@gmail.com>
2024-01-15 18:03:19 +0200

    gfgg

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,45 +41,74 @@
             this.PolyMultiplyNumBox = new System.Windows.Forms.TextBox();
             this.PolyMultiplyNumBox2 = new System.Windows.Forms.TextBox();
             this.MultiplyButton2 = new System.Windows.Forms.Button();
+            this.ChartButton2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.PolynomialChart)).BeginInit();
             this.SuspendLayout();
             // 
             // PolynomialChart
             // 
+            this.PolynomialChart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.PolynomialChart.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.PolynomialChart.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            this.PolynomialChart.BorderlineWidth = 7;
+            chartArea1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            chartArea1.BackSecondaryColor = System.Drawing.Color.Black;
+            chartArea1.BorderWidth = 0;
             chartArea1.Name = "ChartArea1";
             this.PolynomialChart.ChartAreas.Add(chartArea1);
             legend1.Enabled = false;
             legend1.Name = "Legend1";
             this.PolynomialChart.Legends.Add(legend1);
-            this.PolynomialChart.Location = new System.Drawing.Point(-2, -13);
+            this.PolynomialChart.Location = new System.Drawing.Point(-20, -7);
+            this.PolynomialChart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PolynomialChart.Name = "PolynomialChart";
+            this.PolynomialChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.BorderWidth = 3;
             series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(192)))), ((int)(((byte)(127)))));
+            series1.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.IsXValueIndexed = true;
+            series1.LabelAngle = 20;
+            series1.LabelBackColor = System.Drawing.Color.Transparent;
+            series1.LabelBorderColor = System.Drawing.Color.Transparent;
+            series1.LabelForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(85)))), ((int)(((byte)(39)))));
             series1.Legend = "Legend1";
+            series1.MarkerBorderColor = System.Drawing.Color.Transparent;
+            series1.MarkerColor = System.Drawing.Color.Transparent;
+            series1.MarkerImageTransparentColor = System.Drawing.Color.Transparent;
+            series1.MarkerSize = 0;
             series1.Name = "Series1";
+            series1.ShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(145)))), ((int)(((byte)(97)))));
+            series1.ShadowOffset = 1;
+            series1.YValuesPerPoint = 2;
             this.PolynomialChart.Series.Add(series1);
-            this.PolynomialChart.Size = new System.Drawing.Size(467, 468);
+            this.PolynomialChart.Size = new System.Drawing.Size(367, 387);
             this.PolynomialChart.TabIndex = 0;
-            this.PolynomialChart.Text = "chart1";
+            this.PolynomialChart.Text = "Polynomial Chart";
             // 
             // PolyTextBox1
             // 
-            this.PolyTextBox1.Location = new System.Drawing.Point(547, 29);
+            this.PolyTextBox1.Location = new System.Drawing.Point(394, 25);
+            this.PolyTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PolyTextBox1.Name = "PolyTextBox1";
-            this.PolyTextBox1.Size = new System.Drawing.Size(177, 22);
+            this.PolyTextBox1.Size = new System.Drawing.Size(145, 20);
             this.PolyTextBox1.TabIndex = 1;
             // 
             // PolyTextBox2
             // 
-            this.PolyTextBox2.Location = new System.Drawing.Point(547, 84);
+            this.PolyTextBox2.Location = new System.Drawing.Point(395, 69);
+            this.PolyTextBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PolyTextBox2.Name = "PolyTextBox2";
-            this.PolyTextBox2.Size = new System.Drawing.Size(177, 22);
+            this.PolyTextBox2.Size = new System.Drawing.Size(144, 20);
             this.PolyTextBox2.TabIndex = 2;
             // 
             // MultiplyButton
             // 
-            this.MultiplyButton.Location = new System.Drawing.Point(729, 29);
+            this.MultiplyButton.Location = new System.Drawing.Point(542, 25);
+            this.MultiplyButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MultiplyButton.Name = "MultiplyButton";
-            this.MultiplyButton.Size = new System.Drawing.Size(28, 22);
+            this.MultiplyButton.Size = new System.Drawing.Size(21, 20);
             this.MultiplyButton.TabIndex = 3;
             this.MultiplyButton.Text = "X";
             this.MultiplyButton.UseVisualStyleBackColor = true;
@@ -87,9 +116,10 @@
             // 
             // AdditionButton
             // 
-            this.AdditionButton.Location = new System.Drawing.Point(547, 135);
+            this.AdditionButton.Location = new System.Drawing.Point(352, 109);
+            this.AdditionButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.AdditionButton.Name = "AdditionButton";
-            this.AdditionButton.Size = new System.Drawing.Size(253, 27);
+            this.AdditionButton.Size = new System.Drawing.Size(245, 22);
             this.AdditionButton.TabIndex = 4;
             this.AdditionButton.Text = "addition";
             this.AdditionButton.UseVisualStyleBackColor = true;
@@ -97,11 +127,12 @@
             // 
             // ChartButton
             // 
-            this.ChartButton.Location = new System.Drawing.Point(471, 29);
+            this.ChartButton.Location = new System.Drawing.Point(352, 25);
+            this.ChartButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ChartButton.Name = "ChartButton";
-            this.ChartButton.Size = new System.Drawing.Size(70, 22);
+            this.ChartButton.Size = new System.Drawing.Size(38, 20);
             this.ChartButton.TabIndex = 5;
-            this.ChartButton.Text = "chart";
+            this.ChartButton.Text = "Chart";
             this.ChartButton.UseVisualStyleBackColor = true;
             this.ChartButton.Click += new System.EventHandler(this.ChartButton_Click);
             // 
@@ -109,41 +140,57 @@
             // 
             this.AnswerLabel.AutoSize = true;
             this.AnswerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.AnswerLabel.Location = new System.Drawing.Point(547, 292);
+            this.AnswerLabel.Location = new System.Drawing.Point(351, 161);
+            this.AnswerLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.AnswerLabel.Name = "AnswerLabel";
-            this.AnswerLabel.Size = new System.Drawing.Size(127, 39);
+            this.AnswerLabel.Size = new System.Drawing.Size(102, 31);
             this.AnswerLabel.TabIndex = 6;
             this.AnswerLabel.Text = "answer";
             // 
             // PolyMultiplyNumBox
             // 
-            this.PolyMultiplyNumBox.Location = new System.Drawing.Point(762, 29);
+            this.PolyMultiplyNumBox.Location = new System.Drawing.Point(567, 25);
+            this.PolyMultiplyNumBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PolyMultiplyNumBox.Name = "PolyMultiplyNumBox";
-            this.PolyMultiplyNumBox.Size = new System.Drawing.Size(38, 22);
+            this.PolyMultiplyNumBox.Size = new System.Drawing.Size(30, 20);
             this.PolyMultiplyNumBox.TabIndex = 7;
             // 
             // PolyMultiplyNumBox2
             // 
-            this.PolyMultiplyNumBox2.Location = new System.Drawing.Point(762, 85);
+            this.PolyMultiplyNumBox2.Location = new System.Drawing.Point(567, 70);
+            this.PolyMultiplyNumBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PolyMultiplyNumBox2.Name = "PolyMultiplyNumBox2";
-            this.PolyMultiplyNumBox2.Size = new System.Drawing.Size(38, 22);
+            this.PolyMultiplyNumBox2.Size = new System.Drawing.Size(30, 20);
             this.PolyMultiplyNumBox2.TabIndex = 8;
             // 
             // MultiplyButton2
             // 
-            this.MultiplyButton2.Location = new System.Drawing.Point(729, 84);
+            this.MultiplyButton2.Location = new System.Drawing.Point(542, 69);
+            this.MultiplyButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MultiplyButton2.Name = "MultiplyButton2";
-            this.MultiplyButton2.Size = new System.Drawing.Size(28, 23);
+            this.MultiplyButton2.Size = new System.Drawing.Size(21, 21);
             this.MultiplyButton2.TabIndex = 9;
             this.MultiplyButton2.Text = "X";
             this.MultiplyButton2.UseVisualStyleBackColor = true;
             this.MultiplyButton2.Click += new System.EventHandler(this.MultiplyButton2_Click);
             // 
+            // ChartButton2
+            // 
+            this.ChartButton2.Location = new System.Drawing.Point(352, 69);
+            this.ChartButton2.Name = "ChartButton2";
+            this.ChartButton2.Size = new System.Drawing.Size(38, 21);
+            this.ChartButton2.TabIndex = 10;
+            this.ChartButton2.Text = "Chart";
+            this.ChartButton2.UseVisualStyleBackColor = true;
+            this.ChartButton2.Click += new System.EventHandler(this.ChartButton2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(607, 366);
+            this.Controls.Add(this.ChartButton2);
             this.Controls.Add(this.MultiplyButton2);
             this.Controls.Add(this.PolyMultiplyNumBox2);
             this.Controls.Add(this.PolyMultiplyNumBox);
@@ -154,7 +201,12 @@
             this.Controls.Add(this.PolyTextBox2);
             this.Controls.Add(this.PolyTextBox1);
             this.Controls.Add(this.PolynomialChart);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(623, 405);
+            this.MinimumSize = new System.Drawing.Size(623, 405);
             this.Name = "Form1";
+            this.ShowIcon = false;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.PolynomialChart)).EndInit();
@@ -175,6 +227,7 @@
         private System.Windows.Forms.TextBox PolyMultiplyNumBox;
         private System.Windows.Forms.TextBox PolyMultiplyNumBox2;
         private System.Windows.Forms.Button MultiplyButton2;
+        private System.Windows.Forms.Button ChartButton2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -25,62 +25,67 @@ namespace Polynomials
             //mutliply polynomial with num
             //polynomial additionn
             //graph a polynomial
-            Polynomial PolyNums = new Polynomial(1, 2, 3);
-            Polynomial PolyNums2 = new Polynomial(0, 1);
 
-            var Multiplication = PolyNums * 3;
+            //Polynomial PolyNums = new Polynomial(1, 2, 3);
+            //Polynomial PolyNums2 = new Polynomial(0, 1);
 
-            var Addition = PolyNums + PolyNums2;
+            //var Multiplication = PolyNums * 3;
 
+            //var Addition = PolyNums + PolyNums2;
+
+            //for (int i = -20; i <= 20; i++)
+            //{
+            //    PolynomialChart.Series[0].Points.Add(PolyNums.Evaluate(i));
+            //}
+
+            Console.WriteLine("thingamajig");
+        }
+
+        public double[] GetCoefficient(string input)
+        {
+            string[] values = input.Split(' ');
+            double[] Coefficients = new double[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                Coefficients[i] = double.Parse(values[i]); // * double.Parse(numBoxText);
+            }
+            return Coefficients;
+        }
+
+        public void DrawChart(Polynomial PolyNums)
+        {
+            PolynomialChart.Series[0].Points.Clear();
             for (int i = -20; i <= 20; i++)
             {
                 PolynomialChart.Series[0].Points.Add(PolyNums.Evaluate(i));
             }
         }
 
-        public void MultiplyWithNumber(string input, string numBoxText)
-        {
-            string[] values = input.Split(' ');
-            double[] coefficients = new double[values.Length];
-            for (int i = 0; i < values.Length; i++)
-            {
-                coefficients[i] = double.Parse(values[i]);
-            }
-            Polynomial PolyNums = new Polynomial(coefficients);
-
-            var Answer = PolyNums * int.Parse(numBoxText);
-            AnswerLabel.Text = Answer.ToString();
-        }
-
-
         private void MultiplyButton_Click(object sender, EventArgs e)
         {
-            MultiplyWithNumber(PolyTextBox1.Text, PolyMultiplyNumBox.Text);
+            var Coefficients = GetCoefficient(PolyTextBox1.Text);
+
+            Polynomial PolyNums = new Polynomial(Coefficients);
+            var Answer = PolyNums * int.Parse(PolyMultiplyNumBox.Text);
+            AnswerLabel.Text = Answer.ToString();
         }
 
         private void MultiplyButton2_Click(object sender, EventArgs e)
         {
-            MultiplyWithNumber(PolyTextBox2.Text, PolyMultiplyNumBox2.Text);
+            var Coefficients = GetCoefficient(PolyTextBox2.Text);
+
+            Polynomial PolyNums = new Polynomial(Coefficients);
+            var Answer = PolyNums * int.Parse(PolyMultiplyNumBox2.Text);
+            AnswerLabel.Text = Answer.ToString();
         }
 
         private void AdditionButton_Click(object sender, EventArgs e)
         {
-            string[] values = PolyTextBox1.Text.Split(' ');
-            double[] coefficients = new double[values.Length];
-            for (int i = 0; i < values.Length; i++)
-            {
-                coefficients[i] = double.Parse(values[i]);
-            }
+            var Coefficient1 = GetCoefficient(PolyTextBox1.Text);
+            var Coefficient2 = GetCoefficient(PolyTextBox2.Text);
 
-            string[] values2 = PolyTextBox1.Text.Split(' ');
-            double[] coefficients2 = new double[values2.Length];
-            for (int i = 0; i < values2.Length; i++)
-            {
-                coefficients2[i] = double.Parse(values2[i]);
-            }
-
-            Polynomial PolyNums = new Polynomial(coefficients);
-            Polynomial PolyNums2 = new Polynomial(coefficients2);
+            Polynomial PolyNums = new Polynomial(Coefficient1);
+            Polynomial PolyNums2 = new Polynomial(Coefficient2);
 
             var Answer = PolyNums + PolyNums2;
             AnswerLabel.Text = Answer.ToString();
@@ -88,27 +93,16 @@ namespace Polynomials
 
         private void ChartButton_Click(object sender, EventArgs e)
         {
-            string[] values = PolyTextBox1.Text.Split(' ');
-            double[] coefficients = new double[values.Length];
-            for (int i = 0; i < values.Length; i++)
-            {
-                coefficients[i] = double.Parse(values[i]);
-            }
+            var Coefficient = GetCoefficient(PolyTextBox1.Text);
+            Polynomial PolyNums = new Polynomial(Coefficient);
+            DrawChart(PolyNums);
+        }
 
-            string[] values2 = PolyTextBox1.Text.Split(' ');
-            double[] coefficients2 = new double[values2.Length];
-            for (int i = 0; i < values2.Length; i++)
-            {
-                coefficients2[i] = double.Parse(values2[i]);
-            }
-
-            Polynomial PolyNums = new Polynomial(coefficients);
-            Polynomial PolyNums2 = new Polynomial(coefficients2);
-
-            for (int i = -20; i <= 20; i++)
-            {
-                PolynomialChart.Series[0].Points.Add(PolyNums.Evaluate(i));
-            }
+        private void ChartButton2_Click(object sender, EventArgs e)
+        {
+            var Coefficient = GetCoefficient(PolyTextBox2.Text);
+            Polynomial PolyNums = new Polynomial(Coefficient);
+            DrawChart(PolyNums);
         }
     }
 }
